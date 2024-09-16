757d9a0db2463c30a054147e90d56fcb6ecd7c07
RuhamaMalik <malikruhama7@gmail.com>
2024-09-16 13:03:20 +0500

    Sections Merge ( Revision )

---
+++ javascript/class06(Revision)/index.js
@@ -0,0 +1,50 @@
+// var num = 3; // 6
+
+// var myExp =  num++ + ++num + num++ ;
+//  //           3    +    5  +  5
+       
+// console.log(num); // 6
+// console.log(myExp);  // 13
+
+
+// var i = 7; // 11
+// var myExp2 = i++ - i++ + 7 - ++i + ++i - 11;
+// //           7   - 8   + 7 -  10 +  11 - 11
+
+// console.log(i); //11
+// console.log(myExp2);  // -4
+
+
+// var x = 7;
+
+// // x = x + 3;
+// //    x += 3;
+//    x -= 3;
+// console.log(x); 
+
+
+// var fName = "Hajra";
+// var lName = "Asif";
+
+// console.log(fName            + "    " +   lName);
+// console.log(10 + '10'); //1010
+// console.log(10 + 7 + '10'); //1710 
+// console.log('2' + 3 - true + '1'); // 221 231 
+// console.log('9' - 4); // 5
+
+
+
+// var userInput  = prompt("What is JS ?");
+// console.log(userInput);
+
+
+// var question = "What is JS ?";
+// var dAns = "Programmin Language";
+// var userInput  = prompt(question , dAns);
+// console.log(userInput);
+
+// var city = prompt("Where do you live ? ");
+// console.log( city);
+
+//  var userNum = +prompt("What is your lucky number? ");
+// console.log(userNum + 5);
