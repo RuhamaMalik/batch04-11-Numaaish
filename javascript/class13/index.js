9123de2cfd9d6bfd025af629476dbf3846fe90f0
RuhamaMalik <malikruhama7@gmail.com>
2024-10-02 13:39:05 +0500

    Examples ,,, 20 chapters complete

---
+++ javascript/class13/index.js
@@ -0,0 +1,100 @@
+// document.write(`
+//     <div class="card">
+//   <img src="https://jocular-truffle-af613b.netlify.app/images/rainy.gif" alt="Avatar" style="width:100%">
+//   <div class="container">
+//     <h4><b>John Doe</b></h4>
+//     <p>Architect & Engineer</p>
+//   </div>
+// </div>
+
+//     `)
+
+// for (var i = 1; i < 7; i++) {
+//   // console.log(i);
+//   if (i === 3 || i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// var numbers = [78, 107, 33, 65, 25]; // > 50
+
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbers[i] <= 50) {
+//     continue;
+//   }
+
+//   console.log(numbers[i])
+// }
+
+/// print counting
+
+// // for(var i = 1; i <= 100; i = i+10){
+// //          21
+// for(var i = 1; i <= 100; i += 10){
+// // document.write(i + "</br>");
+// //         21       31
+// for(var j= i; j < i + 10; j++ ){
+//     document.write(" " +  j + " " );
+
+// }
+// document.write("</br>")
+// }
+
+//  Print Table
+
+// for(var i=1; i<=10; i++){
+//     // console.log("2 X 2 = 4");
+//     document.write( "5  X  " +  i + " = " +( 5*i) +  "</br>")
+// }
+
+// var startFrom = +prompt("Enter Start Number")
+// var endFrom = +prompt("Enter Ending Number")
+// var tableOf = +prompt("Enter The Number ")
+
+// for(var i= startFrom; i<=endFrom; i++){
+//     // console.log("2 X 2 = 4");
+//     document.write( tableOf + "  X  " +  i + " = " +( tableOf*i) +  "</br>")
+// }
+
+//  Backword
+// for(var i = 10 ; i>=1 ; i--){
+//     console.log(i);
+
+// }
+
+// for(var i = 100; i >= 1; i -= 10){
+// // document.write(i + "</br>");
+// //         21       31
+// for(var j= i; j > i - 10; j-- ){
+//     document.write(" " +  j + " " );
+
+// }
+// document.write("</br>")
+// }
+
+////////////////////
+
+// 1) task pdf   (1 -20 chapters) >>>.  wednesday
+// 2) weather card ( prompt --- if, else if , else)    >>>> sunday
+// 3) Kidz Math Solution (
+//   prompt(table, fCounting, bCounting)
+//  --- if, else if , else)    >>>> friday
+
+// //////////////// Flag
+
+// var subjects = ["Eng", "Urd", "Phy", "Che", "Com"];
+
+// var user_input = prompt("What subject you want.?");
+// var flag = "NO";
+// for (var i = 0; i < subjects.length; i++) {
+//   if (user_input === subjects[i]) {
+//     flag = "YES";
+//     alert(" Avalaible");
+//     break;
+//   }
+// }
+
+// if (flag === "NO") {
+//   alert("Not Avalaible");
+// }
