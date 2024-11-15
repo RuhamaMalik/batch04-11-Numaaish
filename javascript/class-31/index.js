a64848039084763cd565e7d4fd76e693194f7bfe
RuhmaMalik <malikruhama7@gmail.com>
2024-11-15 11:57:00 +0500

    methods

---
+++ javascript/class-31/index.js
@@ -0,0 +1,36 @@
+// function myFunc(a , b){
+// console.log(arguments.length);
+// console.log(myFunc.length);
+
+// }
+
+// myFunc("text 10")
+
+// var student = {
+//   name: "Ruhama",
+//   marks: [80, 75, 91, 100],
+
+//   calculateTotal: function () {
+//     var total = 0;
+//     for (var i = 0; i < this.marks.length; i++) {
+//       total += this.marks[i];
+//     }
+
+//     return total;
+//   },
+
+//   hasPassed: function () {
+//     var total = this.calculateTotal();
+//     if (total >= 250) {
+//       return "Pass";
+//     } else {
+//       return "Fail";
+//     }
+//   },
+// };
+
+
+// console.log(" Total Marks ", student.calculateTotal());
+// console.log(" Result ", student.hasPassed());
+
+
