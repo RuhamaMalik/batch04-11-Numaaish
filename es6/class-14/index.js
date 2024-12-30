080e22220fccd13e9c58d719f75d7b05bc3a7247
RuhamaMalik <malikruhama7@gmail.com>
2024-12-30 14:07:21 +0500

    try catch, promises, fetch method

--- es6/class-14/index.js
+++ es6/class-14/index.js
@@ -37,9 +37,7 @@
 
 // console.log("Happy Ending");  // 4
 
-
 ////////////////////////////////////
-
 
 // const waitFor = () => {
 //   return new Promise((resolve,reject) => setTimeout(reject("hello"), 5000));
@@ -60,14 +58,12 @@
 // asyncFunction();
 // console.log("dlfdlfd");
 
-
-
 // const fetchData = async () =>{
 //     try {
 //        const result =  await fetch('https://dummyjson.com/recipes')
 //        const data = await result.json()
 //        console.log(data);
-        
+
 //     } catch (error) {
 //         throw new Error("My Custom Error message")
 //     }
@@ -75,12 +71,42 @@
 
 // fetchData()
 
-fetch("https://dummyjson.com/recipes").then(
-    (res)=> res.json()
-).then(
-    (data)=> console.log(data.recipes)
-    
-).catch(
-    (err)=> console.log(err)
-    
-)
+// fetch("https://dummyjson.com/recipes").then(
+//     (res)=> res.json()
+// ).then(
+//     (data)=> console.log(data.recipes)
+
+// ).catch(
+//     (err)=> console.log(err)
+
+// )
+
+/////////////////////// 
+
+// const fetchUser = new Promise((res, rej) => {
+// //   setTimeout(() => res("User Date Fetch Successfully"), 2000);
+//   setTimeout(() => res({name:"User1"}), 2000);
+// });
+
+// const fetchProduct = new Promise((res, rej) => {
+// //   setTimeout(() => res("Product Date Fetch Successfully"), 2000);
+//   setTimeout(() => res({productName:"Shampoo"}), 2000);
+// });
+
+// const fetchOrder = new Promise((res, rej) => {
+// //   setTimeout(() => res("Order Data Fetch Successfully"), 2000);
+//   setTimeout(() => rej("Order  Fetch Failed"), 2000);
+// });
+
+// Promise.all([fetchUser, fetchProduct, fetchOrder]).then((results) => {
+//   console.log("All Data Fetched");
+//   console.log(results);
+  
+//   console.log("User Promise Data ", results[0]);
+//   console.log("Product Promise Data ", results[1]);
+//   console.log("Order Promise Data ", results[2]);
+// }).catch(
+//     (err)=>{
+//         console.log(err);
+//     }
+// )
