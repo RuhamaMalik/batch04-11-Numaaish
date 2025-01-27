f9b95b8055684a7647619e25e0903d904ceae0e4
RuhamaMalik <malikruhama7@gmail.com>
2025-01-27 13:02:56 +0500

    firebase auth ---- firebase class 3

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,5 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js'
-import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfxVRQ1X2HjbNiqO5A7arnPTC_5X6-oQQ",
@@ -7,12 +12,15 @@ const firebaseConfig = {
   projectId: "github-firebase-1a0f0",
   storageBucket: "github-firebase-1a0f0.firebasestorage.app",
   messagingSenderId: "330932589600",
-  appId: "1:330932589600:web:820de1c839d1210efdd845"
+  appId: "1:330932589600:web:820de1c839d1210efdd845",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-
-export {auth,createUserWithEmailAndPassword , signInWithEmailAndPassword}
+export { auth, 
+  createUserWithEmailAndPassword,
+   signInWithEmailAndPassword,
+   onAuthStateChanged
+   };
--- firebase/index.js
+++ firebase/index.js
@@ -1,22 +1,77 @@
-import {auth,createUserWithEmailAndPassword, signInWithEmailAndPassword } from "./firebase.config.js"
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "./firebase.config.js";
 
-const register = async (e)=>{
-e.preventDefault();
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
+const register = async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
- try {
-//    let userCredintial =  await createUserWithEmailAndPassword(auth, email, password);
-   let userCredintial =  await signInWithEmailAndPassword(auth, email, password);
-   console.log(userCredintial.user);
- } catch (error) {
-    console.log(error);
+  try {
+    let userCredintial = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredintial?.user);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// let signupForm = document.getElementById("signup-form")
+// if(signupForm){
+
+//    document.getElementById("signup-form").addEventListener("submit", register);
+// }
+
+document.getElementById("signup-form")?.addEventListener("submit", register);
+
+const signIn = async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    let userCredintial = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredintial?.user);
+
+    if (userCredintial?.user) window.location.pathname = "/index.html";
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+console.log(window.location.pathname);
+
+// let siginForm = document.getElementById("login-form")
+// if(siginForm){
+//    document.getElementById("login-form").addEventListener("submit", signIn);
+// }
+document.getElementById("login-form")?.addEventListener("submit", signIn);
+
+
+
+
+
+
+
+
+
+onAuthStateChanged(auth, (user) => {
+   if (user) {
+     const uid = user.uid;
+    console.log(user);
     
- }
-
-
-
-}
-
-
-document.getElementById("auth-form").addEventListener("submit", register);
+   } else {
+     console.log("user signed out");
+     
+   }
+ });
