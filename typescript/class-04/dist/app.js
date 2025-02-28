f5d1ac3ca27b9b38850f4581855a6b0a651c8196
RuhamaMalik <malikruhama7@gmail.com>
2025-02-28 12:05:18 +0500

    touples

---
+++ typescript/class-04/dist/app.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("hello from app.ts");
+//# sourceMappingURL=app.js.map
---
+++ typescript/class-04/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// console.log("hello");
+// console.log("hello bacho");
+// console.log("watch mode on");
+////////////// Tuples  --- fixed length array with multiple / same data data type]
+// type Elem = string|number|boolean
+// let students: Elem[]  = ["student1", "student2",76 ]
+// let students: string[]  = ["student1", "student2" ]
+// let errorVar:[number, string] = [404 , "Page Not Found" ]
+// let fixedStrError:[string, string] = ["404" , "Page Not Found" ]
+// // errorVar[2] = "dfdf"
+// errorVar.push("Zabardasti add hojao");
+// console.log(errorVar);
+//# sourceMappingURL=index.js.map
