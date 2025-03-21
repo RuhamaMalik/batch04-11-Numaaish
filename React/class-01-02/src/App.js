51432ec379295872124c19915d3e6f2c6e6991c1
RuhamaMalik <malikruhama7@gmail.com>
2025-03-21 12:06:16 +0500

    class component, lifecycle methods, react fragment, default/name exports, HOF

---
+++ React/class-01-02/src/App.js
@@ -0,0 +1,104 @@
+// import { Component, } from "react";
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+
+//   render() {
+//     return (
+//       <div>hello g</div>
+//     )
+//   }
+
+// }
+// export class App2 extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+
+//   render() {
+//     return (
+//       <div>hello g App2</div>
+//     )
+//   }
+
+// }
+// export class App3 extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+
+//   render() {
+//     return (
+//       <div>hello g App3</div>
+//     )
+//   }
+
+// }
+
+// export default App
+
+
+
+
+
+/// class COmponent
+/// aik file mai  multiple name export or aik default export
+/// jsx --- one parent (compulsory)
+
+// life cycle method ---- class component
+
+// ComponentDidMount()  /// create
+// ComponentDidUpdate()  /// update
+// ComponentWillUnmount()  /// destsroy
+// setState() ---- use to update state in class component
+
+
+
+
+
+
+
+// useEffect()  
+
+
+import React from "react"
+
+function App() {
+  return (
+
+    <>
+
+
+      <div>Hello React</div>
+      <div>Hello React</div>
+      <div>Hello React</div>
+      <div>Hello React</div>
+
+
+    </>
+
+
+    // <React.Fragment>
+
+
+    //   <div>Hello React</div>
+    //   <div>Hello React</div>
+    //   <div>Hello React</div>
+    //   <div>Hello React</div>
+
+
+    // </React.Fragment>
+
+    // <div>
+    // <div>Hello React</div>
+    // <div>Hello React</div>
+    // <div>Hello React</div>
+    // <div>Hello React</div>
+    // </div>
+  )
+}
+
+export default App
