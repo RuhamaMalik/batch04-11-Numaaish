c51c201a0b81c169fc6b62f67b9b1313344d14e0
RuhamaMalik <malikruhama7@gmail.com>
2025-05-12 12:34:34 +0500

    lazy loading

--- React/class-14/src/App.jsx
+++ React/class-14/src/App.jsx
@@ -1,34 +1,76 @@
-////////////// Redux
+// import About from "./components/About"
 
-import { useEffect } from "react"
-import AddTodo from "./components/AddTodo"
-import TodoList from "./components/TodoList"
-import { useDispatch } from "react-redux"
-import { addAllTasks } from "./components/features/todos/todoSlice"
+import React, { lazy, Suspense } from "react"
+import "./loader.css";
+const About = lazy(()=>import("./components/About"))
+
+
+////////////// Lazy Loading
 
 
 function App() {
-  const dispatch = useDispatch()
 
-  useEffect(()=>{
-
-const allTasks = JSON.parse(localStorage?.getItem("todos")) || []
-dispatch(addAllTasks(allTasks))
-// console.log(allTasks);
-
-  },[])
-
+  
 
   return (
     <>
-      <AddTodo/>
-      <TodoList/>
+
+<h1>My Awesome Heading ......</h1>
+
+
+<Suspense fallback={<div className="loader"></div>}   >
+  <About/>
+  <About/>
+  <About/>
+  <About/>
+  <About/>
+</Suspense>
 
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+// ////////////// Redux
+
+
+// import { useEffect } from "react"
+// import AddTodo from "./components/AddTodo"
+// import TodoList from "./components/TodoList"
+// import { useDispatch } from "react-redux"
+// import { addAllTasks } from "./components/features/todos/todoSlice"
+
+
+// function App() {
+//   const dispatch = useDispatch()
+
+//   useEffect(()=>{
+
+// const allTasks = JSON.parse(localStorage?.getItem("todos")) || []
+// dispatch(addAllTasks(allTasks))
+// // console.log(allTasks);
+
+//   },[])
+
+
+//   return (
+//     <>
+//       <AddTodo/>
+//       <TodoList/>
+
+//     </>
+//   )
+// }
+
+// export default App
+
 
 
 
@@ -50,6 +92,7 @@ export default App
 // }
 
 // export default App
+
 
 
 
