3c21dfafa05c7a94feb26216f3ea7a24fb335a7c
RuhamaMalik <malikruhama7@gmail.com>
2025-06-04 13:02:26 +0500

    restFull Api complete, middlewares

--- Backend/class-09/index.js
+++ Backend/class-09/index.js
@@ -6,11 +6,11 @@ const users = require("./MOCK_DATA.json")
 
 /////////// postman
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 
-app.get("", (req,res)=>{
-res.send("Home Page")
+app.get("", (req, res) => {
+     res.send("Home Page")
 })
 
 // app.get("/users", (req,res)=>{
@@ -38,25 +38,71 @@ res.send("Home Page")
 
 
 
-app.get("/users", (req,res)=>{
-res.json(users);
+app.get("/users", (req, res) => {
+     res.json(users);
 });
 
-app.get("/user/:id", (req,res)=>{
-  const user =users.find((user)=> user.id === Number(req.params.id));
-  res.json(user);
-});
+// app.get("/user/:id", (req,res)=>{
+//   const user =users.find((user)=> user.id === Number(req.params.id));
+//   res.json(user);
+// });
+// app.patch("/user/:id", (req,res)=>{
 
+//   res.json({});
+// });
+// app.delete("/user/:id", (req,res)=>{
 
-app.post("/users", (req,res)=>{
-     users.push({...req.body, id:users.length+1})
-     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users) ,(err,data)=>{
-res.json({
-    message:"user created successfully!", 
-    userId:users.length+1}
-)
+//   res.json({});
+// });
+// app.put("/user/:id", (req,res)=>{
+
+//   res.json({});
+// });
+
+app.route("/api/user/:id")
+     .get((req, res) => {
+          const user = users.find((user) => user.id === Number(req.params.id));
+          return res.json(user);
      })
-// res.send("hello");
+     .patch(
+          (req, res) => {
+               const id = req.params.id;
+               const data = req.body;
+               let user = users.find((user) => user.id === Number(id)); // find user
+
+
+               ////// update user
+               for (const key in data) {
+                    if (Object.prototype.hasOwnProperty.call(user, key)) {
+                         user = { ...user, [key]: data[key] }
+
+                    }
+               }
+
+               //// update user in users
+               users[user.id-1] = user;
+
+               /// update data in file
+               fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+                     return res.json({ message: "User Updated Successfully!", user })
+               })
+
+              
+          }
+     )
+
+
+
+app.post("/users", (req, res) => {
+     users.push({ ...req.body, id: users.length + 1 })
+     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+          res.json({
+               message: "user created successfully!",
+               userId: users.length + 1
+          }
+          )
+     })
+     // res.send("hello");
 });
 
 
@@ -65,5 +111,5 @@ res.json({
 
 
 
-app.listen(8000,()=>console.log("server is running on port 8000")
+app.listen(8000, () => console.log("server is running on port 8000")
 )
---
+++ Backend/class-10/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 8000
+
+
+const checkAge = (req, res,next) => {
+    const age = req.query.age;
+
+    if (!age) {
+        res.json({message:"Age is required!", success:false})
+    } else if (age < 18 || age > 75) {
+            res.send({message:"You cannot access the content", success:false})
+    } else{
+        req.country ="China"
+        next()
+    }
+
+}
+
+
+const secondMiddleware=(req, res,next)=>{
+const gender = req.query.gender;
+ if (!gender) {
+        res.json({message:"Gender is required!", success:false})
+    } else if (gender.toLowerCase() === "male") {
+            res.send({message:"Only females can access the content", success:false})
+    }else{
+        next()
+    }
+}
+const thirdMiddleware=(req, res,next)=>{
+const country = req.country;
+ if (!country) {
+        res.json({message:"Oops something went wrong!", success:false})
+    } else if (country.toLowerCase() !== "pakistan") {
+            res.send({message:"Website is not accessible in this region" + req.country, success:false})
+    }else{
+        next()
+    }
+}
+
+
+app.use(checkAge)
+app.use(secondMiddleware)
+app.use(thirdMiddleware)
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/about', (req, res) => res.send('About Page!'));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
