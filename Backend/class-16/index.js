ef7e5041c11b38a6929ccff7a9bba9a10adbc763
RuhamaMalik <malikruhama7@gmail.com>
2025-06-25 13:34:25 +0500

    start mongoose

--- Backend/class-16/index.js
+++ Backend/class-16/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 5055;
+const port = 5055; 
 
 // Middlewares
 // app.use(express.urlencoded({extended:false}))
